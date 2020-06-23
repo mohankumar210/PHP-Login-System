9cdc8618e917964e544bdda9deab81151aae608e
mohankumar210 <mohan.digismart@gmail.com>
2020-06-23 19:35:58 +0530

    login and register page

---
+++ inc/footer.php
@@ -0,0 +1,8 @@
+
+<!-- jQuery is required -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
+
+    <!-- UIkit JS -->
+
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.4/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.4/dist/js/uikit-icons.min.js"></script>
--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php 
+
+	// Allow the config
+	 define('__CONFIG__', true);
+	// Require the config
+	require_once "inc/config.php"; 
+
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -14,40 +23,23 @@
 </head>
 <body>
 	<div class="uk-section uk-container">
-  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
-			<form class="uk-form-stacked js-login">
+  	
+		<?php echo "Hello World. Today is: "; 
 
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Email</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
-			        </div>
-			    </div>
+		echo date("d m Y"); ?>
+		<p>
+			<a href="login.php">Login</a>
+			<a href="register.php">Register</a>
+		</p>
 
-			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Password</label>
-			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
-			        </div>
-			    </div>
-
-			    <div class="uk-margin">
-			        <button class="uk-button uk-button-default" type="submit">Login</button>
-			    </div>
-
-			</form>
-  		</div>
   	</div>
 
+  	
 
 
- <!-- jQuery is required -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
+<?php require_once "inc/footer.php"; ?> 
 
-    <!-- UIkit JS -->
 
-<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.4/dist/js/uikit.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.4/dist/js/uikit-icons.min.js"></script>
 
 </body>
 </html>

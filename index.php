20f202b5b59ae1117e8a666e85d5c4fdea3cfdfb
mohankumar210 <mohan.digismart@gmail.com>
2020-06-23 17:58:14 +0530

    login page

--- index.php
+++ index.php
@@ -1,3 +1,53 @@
-<?php echo "Hello world"; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	
+	<title>Page Title</title>
 
-Test
+    <base href="/" />
+
+	<!-- UIkit CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.4/dist/css/uikit.min.css" />
+
+<!-- UIkit JS -->
+
+</head>
+<body>
+	<div class="uk-section uk-container">
+  		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+			<form class="uk-form-stacked js-login">
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Email</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="email" required='required' placeholder="email@email.com">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Password</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
+			        </div>
+			    </div>
+
+			    <div class="uk-margin">
+			        <button class="uk-button uk-button-default" type="submit">Login</button>
+			    </div>
+
+			</form>
+  		</div>
+  	</div>
+
+
+
+ <!-- jQuery is required -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js"></script>
+
+    <!-- UIkit JS -->
+
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.4/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.5.4/dist/js/uikit-icons.min.js"></script>
+
+</body>
+</html>

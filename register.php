42fd913e8bddbd1766081222abb2d1982dec9825
mohankumar210 <mohan.digismart@gmail.com>
2020-06-24 11:05:44 +0530

    Final Working Files

--- register.php
+++ register.php
@@ -1,31 +1,32 @@
 <?php 
 
 	// Allow the config
-	 define('__CONFIG__', true);
+	define('__CONFIG__', true);
 	// Require the config
 	require_once "inc/config.php"; 
 
 ?>
 
 <!DOCTYPE html>
-<html>
-<head>
-	
-	<title>Page Title</title>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="robots" content="follow">
+
+    <title>Page Title</title>
 
     <base href="/" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.24/css/uikit.min.css" />
+  </head>
 
-	<!-- UIkit CSS -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.4/dist/css/uikit.min.css" />
+  <body>
 
-<!-- UIkit JS -->
-
-</head>
-<body>
-	<div class="uk-section uk-container">
+  	<div class="uk-section uk-container">
   		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
 			<form class="uk-form-stacked js-register">
-
+				
 				<h2>Register</h2>
 
 			    <div class="uk-margin">
@@ -36,11 +37,13 @@
 			    </div>
 
 			    <div class="uk-margin">
-			        <label class="uk-form-label" for="form-stacked-text">Password</label>
+			        <label class="uk-form-label" for="form-stacked-text">Passphrase</label>
 			        <div class="uk-form-controls">
-			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your Password">
+			            <input class="uk-input" id="form-stacked-text" type="password" required='required' placeholder="Your passphrase">
 			        </div>
 			    </div>
+
+			    <div class="uk-margin uk-alert uk-alert-danger js-error" style='display: none;'></div>
 
 			    <div class="uk-margin">
 			        <button class="uk-button uk-button-default" type="submit">Register</button>
@@ -50,10 +53,6 @@
   		</div>
   	</div>
 
-
-<?php require_once "inc/footer.php"; ?> 
-
-
-
-</body>
+  	<?php require_once "inc/footer.php"; ?> 
+  </body>
 </html>
